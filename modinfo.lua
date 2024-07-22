8bb8e226b9dd7e822668f6001a86a0c1bcc22272
pts1667 <ptstart@zoho.eu>
2024-07-22 05:05:10 +0100

    add planes to no-weapon blacklist

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@ return {
 	name = 'Unit Level Ups',
 	description = 'Stat growth, shields, drones, weapons as units level up.',
 	shortname = 'unitlevelups',
-	version = 'v75',
+	version = 'v76',
 	mutator = '1',
 	game = 'Zero-K',
 	shortGame = 'ZK',

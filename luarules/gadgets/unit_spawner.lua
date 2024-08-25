2ddce8202996d8d516e0ed4a93e7d6fc9895e13f
sprunk <spr.ng@o2.pl>
2024-08-25 03:27:49 +0200

    spairs were removed, use regular pairs

--- luarules/gadgets/unit_spawner.lua
+++ luarules/gadgets/unit_spawner.lua
@@ -1415,7 +1415,7 @@ else
 function WrapToLuaUI()
 	if (Script.LuaUI('ChickenEvent')) then
 		local chickenEventArgs = {}
-		for k, v in spairs(SYNCED.chickenEventArgs) do
+		for k, v in pairs(SYNCED.chickenEventArgs) do
 			chickenEventArgs[k] = v
 		end
 		Script.LuaUI.ChickenEvent(chickenEventArgs)
